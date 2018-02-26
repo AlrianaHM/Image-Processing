49194e1e3d47425fb4503aa2538be97e89a688ea
AlrianaHM <alrianatcm@gmail.com>
2018-02-26 16:01:17 +0700

    fixing error in HE

---
+++ Java/CountingPalmBunches/src/countingpalmbunches/newHistogram.java
@@ -0,0 +1,51 @@
+package countingpalmbunches;
+
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alriana
+ */
+public class newHistogram {
+    BufferedImage imageH,imageB;
+    File output, inputA, inputB;
+    int w,h;
+    
+    public newHistogram(String inH, String inB, String out){
+        try{
+            
+            System.out.print(".");
+            inputA = new File(inB);
+            imageB = ImageIO.read(inputA);
+            w = imageB.getWidth();
+            h = imageB.getHeight();
+            
+            inputB = new File(inH);
+            imageH = ImageIO.read(inputB);
+            
+            for(int i=0; i<h; i++){
+                for(int j=0; j<w; j++){
+                    Color c = new Color(imageB.getRGB(j, i));
+                    int red = (int)c.getRed();
+                    
+                    Color newColor = new Color(red,red,red);
+                    imageH.setRGB(j, i, newColor.getRGB());
+                    
+                }
+            }
+            output = new File(out);
+            ImageIO.write(imageH, "jpg", output);
+            System.out.print(".");
+        } catch (Exception e) {System.out.print(e);}
+    }
+}
