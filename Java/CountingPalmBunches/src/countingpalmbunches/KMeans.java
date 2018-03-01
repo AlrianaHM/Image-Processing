543584c0c3beee3a0371367227b050489db51b39
AlrianaHM <alrianatcm@gmail.com>
2018-03-01 15:37:50 +0700

    add density for selection

--- Java/CountingPalmBunches/src/countingpalmbunches/KMeans.java
+++ Java/CountingPalmBunches/src/countingpalmbunches/KMeans.java
@@ -13,6 +13,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
+import static java.lang.Math.pow;
+import java.util.Arrays;
 import java.util.List;
 import javax.imageio.ImageIO;
 import weka.clusterers.ClusterEvaluation;
@@ -30,10 +32,12 @@ public class KMeans {
     Graphics inputGD;
     PrintWriter fo;
     int oval;
+    double[] minX,minY;
+    double[] maxX,maxY;
     
     public KMeans(String fileName, String inImage, String outImage, String out) {
         try{
-            System.out.println(".");
+            //System.out.println(".");
             File input = new File(inImage);
             image = ImageIO.read(input);
             w = image.getWidth();
@@ -45,7 +49,7 @@ public class KMeans {
             inputGD.setColor(Color.YELLOW);
         
             Instances data;
-            Instances centroids,std;
+            Instances centroids,std,member;
             BufferedReader reader = new BufferedReader(new FileReader(fileName));
             data = new Instances(reader);
             reader.close();
@@ -60,26 +64,64 @@ public class KMeans {
             ClusterEvaluation c= new ClusterEvaluation();
             c.setClusterer(SK);
             c.evaluateClusterer(data);
+            
+            int[] assignments = SK.getAssignments();
+            
+            minX = new double[15];
+            Arrays.fill(minX,new Double(w+1));
+            minY = new double[15];
+            Arrays.fill(minY,new Double(h+1));
+            maxX = new double[15];
+            maxY = new double[15];
+            
+            for ( int i = 0; i < assignments.length; i++ ) {
+                double x = data.instance(i).value(0);
+                double y = data.instance(i).value(1);
+                int cidx= assignments[i];
+                //System.out.println(minX[cidx]);
+                if(x<minX[cidx]){
+                    minX[cidx]=x;
+                }
+                if(y<minY[cidx]){
+                    minY[cidx]=y;
+                }
+                if(x>maxX[cidx]){
+                    maxX[cidx]=x;
+                }
+                if(y>maxY[cidx]){
+                    maxY[cidx]=y;
+                }
+            }
+            int[] dA= new int[15];
+            int[] dB= new int[15];
+            int[] area= new int[15];
+            
+            for ( int i = 0; i < 15; i++ ) {
+                dA[i] = (int) (maxX[i]-minX[i]);
+                dB[i] = (int) (maxY[i]-minY[i]);
+                area[i]= dA[i]*dB[i];
+            }
+            
             oval = 0;
             std= SK.getClusterStandardDevs();
             centroids = SK.getClusterCentroids();
             double[] clusterSize = SK.getClusterSizes();
-            fo.println(c.clusterResultsToString());
+            //fo.println(c.clusterResultsToString());
             for ( int i = 0; i < centroids.numInstances(); i++ ) {
                 Instance inst = centroids.instance( i );
                 Instance s= std.instance(i);
-                if ( clusterSize[i]>2500 ){
+                //System.out.println((int)clusterSize[i]);
+                double dens = clusterSize[i]/area[i];
+                System.out.println(dens*100);
+                
+                if ( dens*100>=25 && clusterSize[i]>=2000){
+                    //System.out.println(area[i] +" | "+ (int)clusterSize[i] +" | "+ dens*100);
                     Double x = inst.value( 0 );
                     Double y = inst.value( 1 );
-                    int sx=(int)s.value(0);
-                    int sy=(int)s.value(1);
-                    if(sx<50 && sy<50){
-                        //System.out.print(i+": ");
-                        //fo.println( i + " x:" + x.intValue() +", y: "+ y.intValue() );
-                        oval++;
-                        BufferedImage bmp = createBitmap(x.intValue(),y.intValue());
-                        
-                    }
+                    
+                    oval++;
+                    BufferedImage bmp = createBitmap(x.intValue(),y.intValue(), dA[i], dB[i]);
+
                 }
             }
             
@@ -97,18 +139,16 @@ public class KMeans {
 
     }
     
-    private BufferedImage createBitmap(int x, int y){
+    private BufferedImage createBitmap(int x, int y, int da, int db){
         //System.out.println(x+"x"+y);
         
         BufferedImage bmp = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
         int a=x-25;
         int b=y-25;
-        int d=50;
         //System.out.println(a+" "+ b+" "+ c+" "+ d);
         for(int i=0;i<5;i++){
-            if(x-25 <= 70 || w-x<=70) d= x-25;
-            if(y-25 <= 70 || h-y<=70) d= x-25;
-            inputGD.drawOval(y-i-25, x-i-25, 50+i*2, 50+i*2);
+            inputGD.drawOval(y-i-db/2, x-i-da/2, db+i*2, da+i*2);
+            
             inputGD.setColor(Color.YELLOW);
         }         
             
